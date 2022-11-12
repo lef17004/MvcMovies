980512b25feed51ac91d58b7dc40ae3569ce00d5
Michael LeFevre <64995133+lef17004@users.noreply.github.com>
2022-11-12 09:18:15 -0700

    Project

---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models;
+
+public class MovieGenreViewModel
+{
+    public List<Movie>? Movies { get; set; }
+    public SelectList? Genres { get; set; }
+    public string? MovieGenre { get; set; }
+    public string? SearchString { get; set; }
+    public string? Sort { get; set;  }
+    //public bool Sort
+    //{
+    //    set
+    //    {
+    //        if (value)
+    //        {
+    //            Movies.OrderByDescending(m => m.ReleaseDate);
+    //            _set = true;
+    //        }
+                
+    //        else
+    //        {
+    //            Movies.OrderBy(m => m.Title);
+    //            _set = false;
+    //        }
+                
+    //    }
+
+    //    get
+    //    {
+    //        return _set;
+    //    }
+
+    //}
+
+    //private bool _set = false;
+
+    public static string[] options = {
+
+    "Scary"
+};
+
+}
+
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+//using MvcMovie.Data;
+using MvcMovieData;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Rating = "G",
+                        Price = 7.99M,
+                        Url = "https://flxt.tmsimg.com/assets/p11747_p_v8_as.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 8.99M,
+                        Url = "https://upload.wikimedia.org/wikipedia/en/2/2f/Ghostbusters_%281984%29_theatrical_poster.png"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Rating = "PG-13",
+                        Price = 9.99M,
+                        Url = "https://resizing.flixster.com/8nnRgW4_nBak9afhfsXUDN130_E=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzNjODk4YjM4LTBjNmUtNGMwMS04MWFmLWIyZWQ4OTIxNjkyMC53ZWJw"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Rating = "R",
+                        Price = 3.99M,
+                        Url = "https://www.themoviedb.org/t/p/w500/4gI4KKmoi0d3yfsF71YU3S0I5t8.jpg"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
